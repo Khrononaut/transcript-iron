edbd0ee9823de960683a157d7c1b93377550f929
Khrononaut <w.felk@me.com>
2021-10-19 13:26:40 +0300

    fixed the issue that some areas would not colur them while some buttons being hovered

--- scripts/colouring-btns.js
+++ scripts/colouring-btns.js
@@ -1,4 +1,4 @@
-const cleanBtn = document.querySelector("#clean-btn");
+
 const cleanBtnDiv = document.querySelector("#clean-btn-div");
 const cleanText = document.querySelector("#clean-text");
 
@@ -6,12 +6,12 @@ cleanBtn.onmouseover = function () {
   cleanBtnDiv.style.borderColor = "#FFA6D5";
   cleanText.style.color = "#FFA6D5";
 }
+
 cleanBtn.onmouseout = function () {
-  cleanBtnDiv.style.borderColor = "#D8D8D8"
-  cleanText.style.color = "white"
+  cleanBtnDiv.style.borderColor = "#D8D8D8";
+  cleanText.style.color = "white";
 }
 
-const copyBtn = document.querySelector("#copy-btn");
 const copyBtnDiv = document.querySelector("#copy-btn-div");
 const copyText = document.querySelector("#copy-text");
 
@@ -20,6 +20,6 @@ copyBtn.onmouseover = function () {
   copyText.style.color = "#FC8621";
 }
 copyBtn.onmouseout = function () {
-  copyBtnDiv.style.borderColor = "#D8D8D8"
-  copyText.style.color = "white"
+  copyBtnDiv.style.borderColor = "#D8D8D8";
+  copyText.style.color = "white";
 }
